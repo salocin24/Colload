1ab52840fe2b0324d425f715896236eeff1f4a2f
salocin24 <66540679+salocin24@users.noreply.github.com>
2021-02-11 20:43:08 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+
+namespace Colload
+{
+    public class Class1 : BaseScript
+    {
+
+        public Class1()
+        {
+            Tick += OnTick;
+            EventHandlers["playerSpawned"] += new Action<Vector3>(playerSpawned);
+        }
+
+        private async Task OnTick()
+        {
+            await Delay(60000);
+        }
+
+        private void playerSpawned([FromSource] Vector3 spawn)
+        {
+            Game.PlayerPed.Weapons.RemoveAll();
+            Game.PlayerPed.Weapons.Give(WeaponHash.AssaultRifleMk2, 100, false, true);
+            Game.PlayerPed.Weapons.Give(WeaponHash.BullpupRifleMk2, 100, false, true);
+            Game.PlayerPed.Weapons.Give(WeaponHash.SMGMk2, 100, false, true);
+            Game.PlayerPed.Weapons.Give(WeaponHash.SpecialCarbineMk2, 100, false, true);
+            Game.PlayerPed.Weapons.Give(WeaponHash.CombatMGMk2, 100, false, true);
+            Game.PlayerPed.Weapons.Give(WeaponHash.HeavySniperMk2, 100, false, true);
+            Game.PlayerPed.Weapons.Give(WeaponHash.MarksmanRifleMk2, 100, false, true);
+            Game.PlayerPed.Weapons.Give(WeaponHash.PumpShotgunMk2, 100, false, true);
+            Game.PlayerPed.Weapons.Give(WeaponHash.RevolverMk2, 100, false, true);
+            Game.PlayerPed.Weapons.Give(WeaponHash.CarbineRifleMk2, 100, false, true);
+            Game.PlayerPed.Weapons.Give(WeaponHash.PistolMk2, 100, false, true);
+            Game.PlayerPed.Weapons.Give(WeaponHash.SNSPistolMk2, 100, false, true);
+            
+
+
+            TriggerEvent("chat:addMessage", new
+            {
+                color = new[] {255,0,0},
+                multiline = true,
+                args = new[] { "[LOADOUT]", "^3You got the weapons chief" }
+            });
+        }
+
+    }
+}
+
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games {"gta5"}
+
+author 'epicturtle'
+description 'Gives you guns'
+version '1.0.0'
+
+client_script {
+'Colload.net.dll'
+}
